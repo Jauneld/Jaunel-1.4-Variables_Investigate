cf38a66adb826947efb69f2b93e537fae6e1ee92
Jdeans@2025 <144694485+Jauneld@users.noreply.github.com>
2024-03-17 01:10:32 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,65 @@
+#Jaunel Deans
+#September 18, 2023
+#We are investigating with is a wrong with the code and answer the questions given. 
+### Example Code 1
+
+fName = "Mr"
+lName = "Colley"
+
+print(fName)
+
+# Identify a variable in the code
+ # Answer: fName & lName
+
+# Identify a string in the code
+ # Answer: "Mr" & "Colley"
+
+# Line 4 is changed to lName = "Thorpe".  How does this affect the output?
+ #Answer: There is no output because the print statement called for fName not lName
+#We would not see the "Thorpe."
+
+# Line 3 is changed to fName = "Mrs". How does this affect the output?
+ # Answer: The output would be Mrs. because the print statment called for fName and show
+# What is in the variable which is Mrs.
+
+
+### Example Code 2
+
+num1 = 20
+num2 = 5
+
+total1 = num1 + 15
+total2 = num2 * 2
+total3 = num1 - num2
+
+print(total3)
+
+# What will be output by the program?
+ # Answer: 15
+
+
+### Example Code 3
+
+name1 = "Ross" 
+name2 = "Monica" 
+name3 = "Joey" 
+name4 = "Rachel" 
+name5 = "Chandler" 
+ 
+print(name1 + " and " + name4) 
+print(name3) 
+ 
+name3 = "Phoebe" 
+
+
+# How many variables are used in the program?
+ # Answer 3 
+
+# What would be the impact of changing  print(name1, " and ", name4) to print(name1, " and ", name5) ?
+ # Answer The names would have comms and spaces after them
+
+# What is the purpose of the '+' symbol in  print(name1, " and ", name4) 
+ # Answer: to join both variables together
+
+# The line  print(name3) is added to the end of the code.  Explain what it will do.
+ # Answer: It will show what is in variable named name3 which is Phoebe
